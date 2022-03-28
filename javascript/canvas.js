b719f1e0b8c9d3f4f67f6b0e067b73168594c5ab
Laia Markixa <laiamr77@gmail.com>
2022-03-28 18:07:16 +0200

    changes to all scheme, but not ended

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,13 +1,19 @@
 class HangmanCanvas {
   constructor(secretWord) {
+    this.secretWord=secretWord;
     /**
      * @type {HTMLCanvasElement}
      */
     this.context = document.getElementById('hangman').getContext('2d');
     // ... your code goes here
     const ctx=canvas.getContext('2d');
+    document.body.appendChild(canvas);
   }
 
+  StartGame(){
+    this.clear();
+    this.createBoard();
+  }
   /**
    *  method that should clear the canvas, 
    * so every time we start the game we have a clean one. 
@@ -15,9 +21,20 @@ class HangmanCanvas {
    */
   createBoard() {
     // ... your code goes here
-    this.clear();
+    this.addImage();
     this.drawLines();
+    pickWord();
   }
+
+
+  /**
+   * puts image of the hang pole
+   */
+  addImage(){
+    this.img=new Image();
+		this.image.src="images\Hangman-0.png";
+  }
+
 
   /**
    * method that should draw one line for each letter of the secret word. 
@@ -26,60 +43,76 @@ class HangmanCanvas {
   drawLines() {
     // ... your code goes here
     //head
-    ctx.beginPath();
-    ctx.lineWidth=4;
-    ctx.arc(500, 270, 30, 0, Math.PI * 2, true);
-    ctx.strokeStyle="black";
-    ctx.stroke();
-    ctx.closePath();
-
-    //spine
-    ctx.beginPath();
-    ctx.lineWidth=4;
-    ctx.moveTo(500, 300);
-    ctx.lineTo(500, 400);
-    ctx.strokeStyle="black";
-    ctx.stroke();
-    ctx.closePath();
-
+      if(wrongAttempts=1||wrongAttempts==this.secretWord.length){
+      ctx.beginPath();
+      ctx.lineWidth=4;
+      ctx.arc(500, 270, 30, 0, Math.PI * 2, true);
+      ctx.strokeStyle="black";
+      ctx.stroke();
+      ctx.closePath();
+    }else if(wrongAttempts==2||wrongAttempts==this.secretWord.length){
+      //spine
+      ctx.beginPath();
+      ctx.lineWidth=4;
+      ctx.moveTo(500, 300);
+      ctx.lineTo(500, 400);
+      ctx.strokeStyle="black";
+      ctx.stroke();
+      ctx.closePath();
+    }else if(wrongAttempts==3||wrongAttempts==this.secretWord.length){
     //left arm
-    ctx.beginPath();
-    ctx.lineWidth=4;
-    ctx.moveTo(500, 320);
-    ctx.lineTo(450, 320);
-    ctx.strokeStyle="black";
-    ctx.stroke();
-    ctx.closePath();
-
+      ctx.beginPath();
+      ctx.lineWidth=4;
+      ctx.moveTo(500, 320);
+      ctx.lineTo(450, 320);
+      ctx.strokeStyle="black";
+      ctx.stroke();
+      ctx.closePath();
+    }else if(wrongAttempts==4||wrongAttempts==this.secretWord.length){
     //right arm
-    ctx.beginPath();
-    ctx.lineWidth=4;
-    ctx.moveTo(500, 320);
-    ctx.lineTo(550, 320);
-    ctx.strokeStyle="black";
-    ctx.stroke();
-    ctx.closePath();
+      ctx.beginPath();
+      ctx.lineWidth=4;
+      ctx.moveTo(500, 320);
+      ctx.lineTo(550, 320);
+      ctx.strokeStyle="black";
+      ctx.stroke();
+      ctx.closePath();
+    }else if(wrongAttempts==5||wrongAttempts==this.secretWord.length){
+      //right leg
+      ctx.beginPath();
+      ctx.lineWidth=4;
+      ctx.moveTo(500, 400);
+      ctx.lineTo(450, 480);
+      ctx.strokeStyle="black";
+      ctx.stroke();
+      ctx.closePath();
+    }else if(wrongAttempts==6||wrongAttempts==this.secretWord.length){
+      //right leg
+      ctx.beginPath();
+      ctx.lineWidth=4;
+      ctx.moveTo(500, 400);
+      ctx.lineTo(550, 480);
+      ctx.strokeStyle="black";
+      ctx.stroke();
+      ctx.closePath();
+    }
+  } 
 
-    //right leg
-    ctx.beginPath();
-    ctx.lineWidth=4;
-    ctx.moveTo(500, 400);
-    ctx.lineTo(450, 480);
-    ctx.strokeStyle="black";
-    ctx.stroke();
-    ctx.closePath();
-
-    //right leg
-    ctx.beginPath();
-    ctx.lineWidth=4;
-    ctx.moveTo(500, 400);
-    ctx.lineTo(550, 480);
-    ctx.strokeStyle="black";
-    ctx.stroke();
-    ctx.closePath();
+  /**
+   * enters game when "START GAME" button is clicked
+   * toggles between start game screen and game screen
+   */
+  startGameEventListener(){
+    document.addEventListener("click", (event) => {
+			console.log(event);
+			const key = event.key;
+      let element=document.getElementsByTagName("body"); 
+      element.classList.toggle("start-screen");
+      element.classList.toggle("game-screen");
+		});
   }
 
-  writeCorrectLetter(index) {
+ writeCorrectLetter(index) {
     // ... your code goes here
   }
 
@@ -91,11 +124,34 @@ class HangmanCanvas {
     // ... your code goes here
   }
 
-  gameOver() {
-    // ... your code goes here
-  }
 
+  /**
+   * WINNER SCREEN
+   * toggles between game screen and winner screen when game is over and player wins
+   */
   winner() {
     // ... your code goes here
+    let element=document.getElementsByTagName("body");    
+    element.classList.toggle("game-screen");
+    element.classList.toggle("winner-screen");
   }
+
+  /**
+   * GAME OVER SCREEN
+   * toggles between game screen and winner screen when game is over and player looses
+   */
+  gameOver() {
+    // ... your code goes here
+    let element=document.getElementsByTagName("body");    
+    element.classList.toggle("game-screen");
+    element.classList.toggle("game-over-screen");
+
+  }
+
+
+  clear() {
+		this.ctx.clearRect(0, 0, this.gameSize.w, this.gameSize.h);
+	}
+
+
 }
